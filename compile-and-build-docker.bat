d01cf624156c631b4f2ae371cb784ea3c73a4eef
JuanDavid31 <juandavid0306@hotmail.com>
2019-07-24 03:25:19 -0500

    (Refactor) Moviendo valores de la db y jwt key a variables del entorno

--- compile-and-build-docker.bat
+++ compile-and-build-docker.bat
@@ -2,5 +2,8 @@ call mvn clean package
 call docker-compose down
 call docker-compose build ^
 --build-arg ADMIN_EMAIL_ENV=%ADMIN_EMAIL% ^
---build-arg ADMIN_PASS_ENV=%ADMIN_PASS%
+--build-arg ADMIN_PASS_ENV=%ADMIN_PASS% ^
+--build-arg POSTGRES_PASSWORD_ENV=%POSTGRES_PASSWORD% ^
+--build-arg POSTGRES_DB_ENV=%POSTGRES_DB% ^
+--build-arg JWT_KEY_ENV=%JWT_KEY%
 call docker-compose up
