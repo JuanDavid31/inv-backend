25685cb32c096ee08e0bdf016da158452f1702f8
JuanDavid31 <juandavid0306@hotmail.com>
2020-01-10 08:20:18 +0100

    (Refactor) Ajustando nombre SSE

--- src/main/java/rest/sse/DashboardEventSourceServlet.kt
+++ src/main/java/rest/sse/DashboardEventSourceServlet.kt
@@ -4,7 +4,7 @@ import org.eclipse.jetty.servlets.EventSource
 import org.eclipse.jetty.servlets.EventSourceServlet
 import javax.servlet.http.HttpServletRequest
 
-class SseEventSourceServlet: EventSourceServlet() {
+class DashboardEventSourceServlet: EventSourceServlet() {
 
     override fun newEventSource(request: HttpServletRequest): EventSource? {
         val sseEventSource = SseEventSource()
--- src/main/java/usecase/GrupoUseCase.kt
+++ src/main/java/usecase/GrupoUseCase.kt
@@ -48,8 +48,7 @@ class GrupoUseCase(val daoGrupo: DaoGrupo, val daoReaccion: DaoReaccion){
     fun darGruposConReaccionDeUsuario(idProblematica: Int, email: String): MutableList<Grupo> {
         val grupos = daoGrupo.darGrupos(idProblematica)
         val reaccionOptional = daoReaccion.darReaccionEnGrupoPorUsuario(idProblematica, email)
-        reaccionOptional.ifPresent {
-            val reaccion = reaccionOptional.get()
+        reaccionOptional.ifPresent {reaccion ->
             val grupo = grupos.find { it.id == reaccion.idGrupo }
             grupo!!.reaccion = reaccion.valor
             grupo!!.cantidad = 1
